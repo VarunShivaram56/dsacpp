72b19f6347d36667e96e55ac88401d524377b292
VarunShivaram <varunshivaram2000@gmail.com>
2024-09-24 00:28:09 +0530

    Second gfg file to be pushed

---
+++ gfg2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Solution {
+  public:
+    string compareNM(int n, int m){
+        if(n==m)
+            return "equal";
+        else if(n<m)
+            return "lesser";
+        else
+            return "greater";
+        
+        // code here
+    }
+};
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        Solution obj;
+        cout<<obj.compareNM(n,m)<<endl;
+
+    }
+    return 0;
+}
